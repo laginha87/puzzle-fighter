e9dcc77354fe4951edc52e476d32717bac283e57
Filipe Correia <filipe.lp.correia@gmail.com>
2019-05-22 20:58:26 +0100

    Reduces falling speed, Fixes bug on piece move

--- src/logic/PieceLogic.ts
+++ src/logic/PieceLogic.ts
@@ -17,7 +17,7 @@ export class PieceLogic implements Updatable {
     private moving = true;
     public static FALLING_SPEED = 0.007;
     private currentPosition = 0;
-    public fallingSpeed : number;
+    public fallingSpeed: number;
 
     constructor(public blocks: BlockLogic[], public board: BoardLogic) {
         this.position = { x: 0, y: 0 };
@@ -50,14 +50,23 @@ export class PieceLogic implements Updatable {
     }
 
     moveRight() {
+        if (!this.moving) {
+            return;
+        }
         this.moveH(1);
     }
 
     moveLeft() {
+        if (!this.moving) {
+            return;
+        }
         this.moveH(-1);
     }
 
     rotate() {
+        if (!this.moving) {
+            return;
+        }
         const { position } = this;
         const nextPosition = (this.currentPosition + 1) % POSITIONS.length;
         const nextY = (position.y + POSITIONS[nextPosition].y);
@@ -73,6 +82,9 @@ export class PieceLogic implements Updatable {
     }
 
     moveDown() {
+        if (!this.moving) {
+            return;
+        }
         const { position } = this;
         const nextY = (position.y + 1);
 
