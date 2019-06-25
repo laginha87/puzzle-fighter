be4a27c1d34de83447276289592b8e2c0d2e6001
Filipe Correia <filipe.lp.correia@gmail.com>
2019-06-25 21:57:22 +0100

    Gets  the diff blocks from the query string

--- debug/Diff.tsx
+++ debug/Diff.tsx
@@ -3,6 +3,7 @@ import { ClientGame } from 'src/game/ClientGame';
 import { DiffScene } from 'debug/diff/DiffScene';
 import { unserializeBoard } from 'src/serializeBoard';
 import { BoardView } from 'src/view';
+import * as queryString from 'query-string';
 
 
 export class Diff extends React.Component{
@@ -10,19 +11,39 @@ export class Diff extends React.Component{
         const game = new ClientGame('#container');
         game.init();
         const diff = new DiffScene({});
-        const logic = unserializeBoard('10:10|1:9:w:b|2:4:c:r|4:10:n:e');
-        diff.view = new BoardView(logic, {
-            blockSize: {
-                width: 32,
-                height: 32
-            },
-            origin: {
-                x: 40,
-                y: 40
-            }
-        });
 
-        diff.view.scene = diff;
+        const values = queryString.parse(location.search);
+        const logic1 = unserializeBoard(values.b1 as string);
+
+        diff.boards = [
+            new BoardView(logic1, {
+                blockSize: {
+                    width: 32,
+                    height: 32
+                },
+                origin: {
+                    x: 40,
+                    y: 40
+                }
+            }),
+
+        ];
+
+        if(values.b2) {
+            const logic2 = unserializeBoard(values.b2 as string);
+            diff.boards.push(new BoardView(logic2, {
+                blockSize: {
+                    width: 32,
+                    height: 32
+                },
+                origin: {
+                    x: 400,
+                    y: 40
+                }
+            }));
+        }
+
+        diff.boards.forEach((e) => e.scene = diff);
 
         game.view.phaser.scene.add('diff', diff);
         game.view.phaser.scene.start('diff');
--- debug/diff/DiffScene.tsx
+++ debug/diff/DiffScene.tsx
@@ -1,41 +1,43 @@
-import { BoardLogic } from 'src/logic';
 import { BoardView, BlockView } from 'src/view';
 import blocksJson from 'assets/blocks.json';
 import blocksPng from 'assets/blocks.png';
 
 export class DiffScene extends Phaser.Scene {
-    public view!: BoardView;
+    public boards!: BoardView[];
 
     public preload() {
         const img = document.createElement('img');
         img.src = blocksPng;
         this.textures.addAtlasJSONHash('blocks', img, blocksJson);
-        this.view.preload();
+        this.boards.forEach( e => e.preload());
     }
 
     public init() {
-        this.view.init()
+        this.boards.forEach( e => e.init());
     }
 
     public start() {
     }
 
     public create() {
-        this.view.create();
-        Object.values(this.view.logic.blocks).forEach((e) => {
-            const view = new BlockView(e, {width: 32, height: 32});
-            view.scene = this;
-            view.create();
-            // this.view.container.add(view.sprite);
-            view.refresh();
-            e.view = view;
+        this.boards.forEach( (board) =>{
+            board.create();
+            Object.values(board.logic.blocks).forEach((e) => {
+                const view = new BlockView(e, {width: 32, height: 32});
+                view.scene = this;
+                view.create();
+                board.container.add(view.sprite);
+                view.refresh();
+                e.view = view;
 
-            return view;
-        })
+                return view;
+            });
+
+        });
     }
 
 
     public update(a : number, b : number) {
-        this.view.update(a, b);
+        this.boards.forEach( e => e.update(a, b));
     }
 }
